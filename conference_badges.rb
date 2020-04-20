622448fa744508806c2bb119b87139f8189a17e6
guerino antonini <guerantonini94@gmail.com>
2020-04-20 20:12:08 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,12 @@
-# Write your code here.
+def badge_maker(name)
+   "Hello, my name is #{name}."
+end
+
+def batch_badge_creator(speakers)
+  speakers = ["edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
+  
+  count = 0 
+  while count <= speakers.length-1
+    puts "Hello, my name is #{speakers[count]}."
+  end
+end
